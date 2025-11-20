6e2959420ec23ac8f7b6f548313891df57df260e
shoeheng <zshuheng@umich.edu>
2025-11-20 17:12:27 -0500

    Update __init__.py

--- dbmle-pkg/src/dbmle/__init__.py
+++ dbmle-pkg/src/dbmle/__init__.py
@@ -1,6 +1,5 @@
 # src/dbmle/__init__.py
 
-# Re-export the new core API that now lives under the dbmle package.
 from dbmle.core import (
     dbmle,
     dbmle_from_ZD,
@@ -12,4 +11,5 @@ __all__ = [
     "dbmle_from_ZD",
     "DBMLEResult",
 ]
+
 
